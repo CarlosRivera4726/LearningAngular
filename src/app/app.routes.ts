c5555eed5ebfc17f2ca101cbf72c2c70a448b3af
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-04-24 12:00:40 -0500

    configured the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { UserFormComponent } from './components/users/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'user/create', component: UserFormComponent },
+  { path: 'users/:id', component: UserFormComponent },
+  { path: '**', redirectTo: 'home' },
+];
