0927c40d5b1a276d5211f1e6dc6dbeb67584c38f
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-04-24 12:00:06 -0500

    config angular to use css and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
