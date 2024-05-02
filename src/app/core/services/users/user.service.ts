55ab2798d45609dd300e4173d77eb4f99549a7ff
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-02 15:27:10 -0500

    enable to use token access

--- src/app/core/services/users/user.service.ts
+++ src/app/core/services/users/user.service.ts
@@ -7,18 +7,25 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class UserService {
+  options = {
+    headers: {
+      Authorization:
+        'Bearer ' +
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNvbnRyYUBtYWlsLmNvbSIsImlhdCI6MTcxNDY4MDgxOSwiZXhwIjoxNzE5ODY0ODE5fQ.H0KTzoJwcAR8W4towyksONhTeQ2ZCSGxvLTnI7Tu-Y8',
+    },
+  };
   private url: string = 'http://localhost:3000/users';
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<IUser[]> {
-    return this.http.get<IUser[]>(this.url);
+    return this.http.get<IUser[]>(this.url, this.options);
   }
 
   addUser(user: IUser) {
-    return this.http.post<IUser>(this.url, user);
+    return this.http.post<IUser>(this.url, user, this.options);
   }
 
   deleteUser(id: string): Observable<IUser> {
-    return this.http.delete<IUser>(`${this.url}/${id}`);
+    return this.http.delete<IUser>(`${this.url}/${id}`, this.options);
   }
 }
