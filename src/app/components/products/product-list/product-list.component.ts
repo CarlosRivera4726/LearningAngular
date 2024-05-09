5cc7769ab2f1b10d9b7c058187fb311d37193bc5
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-09 16:41:53 -0500

    watching new products

--- src/app/components/products/product-list/product-list.component.ts
+++ src/app/components/products/product-list/product-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../../core/services/products/products.service';
 import { IProduct } from '../../../core/interfaces/products/iproduct';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
@@ -18,15 +19,15 @@ export class ProductListComponent implements OnInit {
   }
 
   getProducts() {
-    this.products = this.productService.getLocalProducts();
-    /*
-        this.productService.getProducts().subscribe({
+    //this.products = this.productService.getLocalProducts();
+
+    this.productService.getProducts().subscribe({
       next: (products) => {
         this.products = products;
       },
       error: (error) => {
         console.log(error);
       },
-    });*/
+    });
   }
 }
