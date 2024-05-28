86f0375d5f5e226b47cfe1fde9e34419ac94ae4d
CarlosRivera4726 <carlosrivera_-717hotmail.com>
2024-05-28 10:40:03 -0500

    ```text chore: Add error alert component to login page
    Update the login component HTML to include the error alert component when the showMessage flag is true. This enhancement improves the user experience by displaying error messages to the user when login fails.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,15 +10,18 @@ import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../core/services/users/user.service';
 import { LoginService } from '../../core/services/login/login.service';
 import { initFlowbite } from 'flowbite';
+import { ErrorAlertComponent } from '../messages/error-alert/error-alert.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, ReactiveFormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, ErrorAlertComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent implements OnInit {
+  protected message: string = '';
+  protected showMessage: boolean = false;
   constructor(private router: Router, private loginService: LoginService) {}
   formLogin!: FormGroup;
 
@@ -53,10 +56,12 @@ export class LoginComponent implements OnInit {
         }
       },
       error: (error) => {
-        alert(
-          'El usuario no se encuentra registrado o la contraseña no es valida'
-        );
-        console.error(error);
+        this.message =
+          'El usuario no se encuentra registrado o la contraseña no es valida.';
+        this.showMessage = true;
+        setTimeout(() => {
+          this.showMessage = false;
+        }, 5000);
       },
     });
   }
