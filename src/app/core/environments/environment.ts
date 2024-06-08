d3484312f82d7e06afc83728485bb9680047c201
CarlosRivera4726 <carlosrivera_-717@hotmail.com>
2024-06-08 18:58:40 -0500

    bug: solved bug to using an url //product

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -3,5 +3,4 @@ export const environment = {
   UPLOAD_PRESET: 'lgiqr6xt',
 };
 
-export const SERVER_URL_DEPLOY =
-  'https://learningnestjs-production-5567.up.railway.app/';
+export const SERVER_URL_DEPLOY = 'https://learningnestjs-production-5567.up.railway.app';
